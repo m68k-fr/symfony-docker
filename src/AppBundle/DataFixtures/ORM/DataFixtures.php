0bbd7b9ef5b2cecaf5983dd6d68f1572cfd763c2
Lionel Le Moullec <llemoullec@gmail.com>
2018-10-19 22:10:51 +0200

    Add better fixture using fzaninotto/Faker

--- src/AppBundle/DataFixtures/ORM/DataFixtures.php
+++ src/AppBundle/DataFixtures/ORM/DataFixtures.php
@@ -5,21 +5,29 @@ namespace AppBundle\DataFixtures\ORM;
 use AppBundle\Entity\Article;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
 class DataFixtures implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+
+        $faker = Faker\Factory::create('fr_FR');
+
         // create 20 fake articles
         for ($i = 1; $i < 21; ++$i) {
             $article = new Article();
-            $article->setTitle('article '.$i);
-            $article->setContent('article '.$i);
-            $article->setPostedAt(new \DateTime('now'));
-            $article->setActive(true);
+            $article->setTitle($faker->sentence);
+
+            $paragraphs = "";
+            foreach ($faker->paragraphs() as $paragraph) {
+                $paragraphs .=  "<p>".$paragraph."</p>\n";
+            }
+            $article->setContent($paragraphs);
+            $article->setPostedAt($faker->dateTime);
+            $article->setActive($faker->boolean);
             $manager->persist($article);
         }
-
         $manager->flush();
     }
 }
